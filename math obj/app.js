3515e9611eb6ac74cc2c85fce6c8968b129c6fe7
Esin Çağla KIRAL <caglakiral@gmail.com>
2022-04-04 17:17:58 +0300

    first commit

---
+++ math obj/app.js
@@ -0,0 +1,12 @@
+// js Math objesi incelenicek, metodları bir kaç örnek yapılacak
+// -https://www.w3schools.com/js/js_math.asp
+
+
+//  JavaScript Math.abs()
+
+let number = -4.6;
+
+console.log(Math.abs(number));
+
+let number2 = 36;
+console.log(Math.sqrt(number2));
